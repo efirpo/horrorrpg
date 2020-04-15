fca0eb4bcefb61034012fa21c0640eb2a002c219
Ethan Firpo <efirpo@gmail.com>
2020-04-15 14:33:50 -0700

    Split game and character and challenges back into separate classes, add character creator method to game class

--- src/lovecraft.js
+++ src/lovecraft.js
@@ -1,26 +1,26 @@
 export class Game {
 
   constructor() {
-    // character: {
-    //   name = "";
-    //   profession = "";
-    //   baseStats: {
-    //     perception;
-    //     grit;
-    //     strength;
-    //     stealth;
-    //     sanity;
-    //   }
-    //   inventory = [];
-    //   level = 1;
-    //   xp = 0;
-    // }
+    this.character = {}
+    this.challenge = {}
+  };
 
-    // challenge: {
-    //   id;
-    //   reqItems = [];
-
-    // }
+  createCharacter(name, profession) {
+    this.character.name = name;
+    this.character.profession = profession;
+    if (this.character.profession = "Artist") {
+      this.character.baseStats = [7, 4, 3, 8, 12];
+      this.character.inventory.push("sketchpad", "ink");
+    } else if (this.characterprofession = "Detective") {
+      this.character.baseStats = [7, 6, 3, 6, 11];
+      this.character.inventory.push("hand-lens", "lighter");
+    } else if (this.character.profession = "Scholar") {
+      this.character.baseStats = [6, 3, 4, 9, 7];
+      this.character.inventory.push("arcane-page", "candle");
+    } else if (this.character.profession = "Soldier") {
+      this.character.baseStats = [6, 8, 6, 2, 8];
+      this.character.inventory.push("stim-pack", "torch");
+    }
   };
   // stat indexes
   // Perception[0]
@@ -30,63 +30,54 @@ export class Game {
   // Sanity[4]
 
 
-  // createCharacter(name, profession) {
-  //   this.character.name = name;
-  //   this.character.profession = profession;
-  //   if (this.character.profession = "Artist") {
-  //     this.character.baseStats.perception = 7;
-  //     this.character.baseStats.grit = 4;
-  //     this.character.baseStats.strength = 3;
-  //     this.character.baseStats.stealth = 8;
-  //     this.character.baseStats.sanity = 12;
-  //     this.character.inventory.push("sketchpad", "ink")
-  //   } else if (this.chracter.profession = "Detective") {
-  //     this.character.baseStats.perception = 7;
-  //     this.character.baseStats.grit = 6;
-  //     this.character.baseStats.strength = 3;
-  //     this.character.baseStats.stealth = 6;
-  //     this.character.baseStats.sanity = 11;
-  //     this.character.inventory.push("hand-lens", "lighter")
-  //   } else if (this.character.profession = "Scholar") {
-  //     this.character.baseStats.perception = 6;
-  //     this.character.baseStats.grit = 3;
-  //     this.character.baseStats.strength = 4;
-  //     this.character.baseStats.stealth = 9;
-  //     this.character.baseStats.sanity = 7;
-  //     this.character.inventory.push("arcane-page", "candle")
-  //   } else if (this.character.profession = "Soldier") {
-  //     this.character.baseStats.perception = 6
-  //     this.character.baseStats.grit = 8
-  //     this.character.baseStats.strength = 6
-  //     this.character.baseStats.stealth = 2
-  //     this.character.baseStats.sanity = 8;
-  //     this.character.inventory.push("stim-pack", "torch")
-  //   }
-  // };
+};
 
-  // levelUp() {
-  //   let xpcheck = (this.xp / 5)
-  //   if (this.character.xp % 5 === 0) {
-  //     this.character.level = xpcheck
-  //   }
-  // }
-
-  // randomEncounter() {
-  //   let encounterChance = Math.ceil(Math.random() * 3);
-  //   if (encounterChance === 1) {
-  //     this.challenge.id = 1
-  //   } else if (encounterChance === 2) {
-  //     this.challenge.id = 2;
-  //   } else if (encounterChance === 3) {
-  //     this.challenge.id = 3;
-  //   }
-
-  //   if (this.challenge.id === 1) {
-
-  //   }
+export class Character {
+  constructor(name, profession) {
+    this.name = name;
+    this.profession = profession;
+    this.baseStats = [];
+    this.inventory = [];
+    this.level = 1;
+    this.xp = 0;
+  };
 
 
-  // }
+};
 
-}
+// export class Challenge {
+//   constructor() {
+//     this.id;
+//     this.reqItems = [];
+//   };
+// };
+
+
+
+
+//   levelUp() {
+//     let xpcheck = (this.xp / 5)
+//     if (this.character.xp % 5 === 0) {
+//       this.character.level = xpcheck
+//     }
+//   }
+
+//   randomEncounter() {
+//     let encounterChance = Math.ceil(Math.random() * 3);
+//     if (encounterChance === 1) {
+//       this.challenge.id = 1
+//     } else if (encounterChance === 2) {
+//       this.challenge.id = 2;
+//     } else if (encounterChance === 3) {
+//       this.challenge.id = 3;
+//     }
+
+//     if (this.challenge.id === 1) {
+
+//     }
+
+
+//   }
+
+
 
